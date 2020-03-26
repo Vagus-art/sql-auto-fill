de46526944d4af9253b7f59703e8026bb21091c6
Vagus-art <agustin.favo@gmail.com>
2020-03-25 22:54:11 -0300

    script done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# sql-auto-fill
-sql query generator created to populate pg-rest-api
+# SQL Auto Fill
+## sql query generator created to populate pg-rest-api
+Makes a request to a name database api, and adds a random number as phone number.
+With this app I can generate a text file with lots of SQL queries, to fill my database directly.
+
+### Usage:
+- `npm install` to download dependencies
+- `npm start` to write data to **data.sql**
---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const axios = require("axios");
+
+const generatePhone = () => { 
+    const minm = 1000000000; 
+    const maxm = 9999999999; 
+    return Math.floor(Math 
+    .random() * (maxm - minm + 1)) + minm; 
+} 
+
+const formatData = (personArray) => {
+    return personArray.map(person=>({
+        name: `${person.name} ${person.surname} `,
+        phone: generatePhone()
+    }));
+}
+
+const generateQueries = async (jsonData) => {
+    let queryStrings = "";
+    await jsonData.forEach(person=>{queryStrings = queryStrings+`\n INSERT INTO contacts (name,phone) VALUES ('${person.name}',${person.phone});`});
+    console.log(queryStrings);
+    return queryStrings;
+}
+
+const getData = async () => {
+    const response = await axios.get('https://uinames.com/api/?region=argentina?amount=400');
+    const formattedResponse = await formatData(response.data);
+    console.log(formattedResponse);
+    return generateQueries(formattedResponse);
+}
+
+const writeFile = async () => {
+    const data = await getData();
+    console.log(data);
+    fs.writeFile("data.sql", data, (err) => {
+        if (err) console.log(err);
+        console.log("Successfully Written to File.");
+      });
+}
+
+writeFile();
